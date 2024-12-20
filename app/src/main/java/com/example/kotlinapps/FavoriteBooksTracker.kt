034b0f997af8651f37ab2e4ca7e410d9b55ea88f
igor <igor.kowalczyk07@poczta.fm>
2024-12-20 20:24:35 +0100

    added method dodaj

--- app/src/main/java/com/example/kotlinapps/FavoriteBooksTracker.kt
+++ app/src/main/java/com/example/kotlinapps/FavoriteBooksTracker.kt
@@ -3,6 +3,20 @@ package com.example.kotlinapps
 class Program {
     val ksiazki = mutableListOf("")
 
+    fun dodaj() {
+        while (true) {
+            println("\npodaj nazwe ksiazki, autora, oraz rok wydania, lub wpisz 'koniec' aby zakonczyc dodawanie: ")
+            val ksiazka = readln()
+
+            if (ksiazka.lowercase() == "koniec") {
+                break
+            } else {
+                ksiazki.add(ksiazka)
+                println("Twoja lista zawiera ksiazki: " + ksiazki)
+
+            }
+        }
+    }
 
 }
 
