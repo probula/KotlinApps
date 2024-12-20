f0c29d2db909ba0136d730dd3fb27cf3f32a3918
igor <igor.kowalczyk07@poczta.fm>
2024-12-20 20:03:14 +0100

    created class Coin

--- app/src/main/java/com/example/kotlinapps/CoinOrDiceRollSimulator.kt
+++ app/src/main/java/com/example/kotlinapps/CoinOrDiceRollSimulator.kt
@@ -2,6 +2,14 @@ interface RollingTool {
     fun roll()
 }
 
+class Coin : RollingTool{
+    override fun roll() {
+        val arr = arrayOf("Heads", "Tails")
+        val randomArr = arr.random()
+        println("Wylosowano: "+randomArr)
+    }
+}
+
 fun main(){
 
 }
