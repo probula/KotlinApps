6e6d32137bdf5c1a92cf94f26af9a7bd2bbabfcc
Damian <damiankontakt7@gmail.com>
2024-12-20 23:38:07 +0100

    Utworzenie funkcji Quiz wykorzystującej pętle by ułatwić iterowanie po pytaniach

--- app/src/main/java/com/example/kotlinapps/KnowledgeQuiz.kt
+++ app/src/main/java/com/example/kotlinapps/KnowledgeQuiz.kt
@@ -15,15 +15,25 @@ class WielokrotnyWybor(val pytanie: String, val opcje: List<String>, val indeksO
         odpowiedz.toIntOrNull() == indeksOdpowiedzi
 }
 
+fun Quiz(pytania: List<Pytanie>) {
+    var wynik = 0
+    for (p in pytania) {
+        println(p.zapytaj())
+        val odpowiedz = readLine() ?: ""
+        if (p.sprawdzOdpowiedz(odpowiedz)) {
+            wynik++
+        }
+    }
+    println("Twój wynik: $wynik/${pytania.size}")
+}
+
 fun main() {
+    println("Odpowiadaj na pytania liczbami od 1-4 lub prawda i fałsz pisane z małych liter")
     val pytania = listOf(
         PrawdaFalsz("Czy Lewandowski jest najlepszym piłkarzem na świecie?", true),
         WielokrotnyWybor("Ile goli strzelił Lewandowski w ciągu 9 min podczas meczu Bayernu Monachium przeciwko Wolfsburgowi w 2015 roku?",
             listOf("1. 5", "2. 1", "3. 4", "4. 3"), 1),
     )
-    for (p in pytania) {
-        println(p.zapytaj())
-        val odpowiedz = readLine() ?: ""
-        println(if (p.sprawdzOdpowiedz(odpowiedz)) "Poprawna odpowiedź!" else "Błędna odpowiedź.")
-    }
+
+    Quiz(pytania)
 }
