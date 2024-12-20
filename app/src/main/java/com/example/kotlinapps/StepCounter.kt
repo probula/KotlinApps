417d11a997b58b5883711cd1d5ff01567e3832ca
igor <igor.kowalczyk07@poczta.fm>
2024-12-20 20:41:30 +0100

    created function krok

--- app/src/main/java/com/example/kotlinapps/StepCounter.kt
+++ app/src/main/java/com/example/kotlinapps/StepCounter.kt
@@ -5,6 +5,23 @@ class Krokomkierz{
     companion object{
         var liczba = 0 //val - stała
     }
+
+    fun krok(){
+        println("Aby zliczac kroki wpisz SPACJE i kliknij ENTER, aby zakonczyc liczenie wpisz 'koniec'")
+        while (true){
+            val input = readln()
+
+            if(input == " "){
+                liczba+=1
+                println("liczba kroków: " + liczba)
+            }
+            else if(input.lowercase() == "koniec"){
+                println("Zakończono zliczanie, łączna liczba kroków:  " + liczba)
+
+            }
+        }
+    }
+
 }
 
 fun main(){
