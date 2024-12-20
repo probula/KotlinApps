d25e490af46c2dbf65df1b935135f9c774c44d29
igor <igor.kowalczyk07@poczta.fm>
2024-12-20 20:09:20 +0100

    added application interface

--- app/src/main/java/com/example/kotlinapps/CoinOrDiceRollSimulator.kt
+++ app/src/main/java/com/example/kotlinapps/CoinOrDiceRollSimulator.kt
@@ -18,5 +18,21 @@ class Dice : RollingTool{
 }
 
 fun main(){
+    println("Rzut moneta - wybierz: 1")
+    println("Rzut kością - wybierz: 2")
+    val wybor = readln()
 
+    if(wybor == "1"){
+        println("Wybrales: Rzut moneta")
+        val coin = Coin()
+        coin.roll()
+    }
+    else if(wybor == "2"){
+        println("Wybrales: Rzut kością")
+        val dice = Dice()
+        dice.roll()
+    }
+    else{
+        println("podano nieprawidłową wartość")
+    }
 }
