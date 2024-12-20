0d080af5bed7f9fcf4c3e20ebba599af4be20929
igor <igor.kowalczyk07@poczta.fm>
2024-12-20 20:13:24 +0100

    created singleton class Quotes

--- app/src/main/java/com/example/kotlinapps/RandomQuoteGenerator.kt
+++ app/src/main/java/com/example/kotlinapps/RandomQuoteGenerator.kt
@@ -1,2 +1,10 @@
 package com.example.kotlinapps
 
+object Quotes { //Singleton
+    val quotesList = mutableListOf(
+        "The pain of discipline is much less than the pain of regret. - David Goggins",
+        "The only way to do great work is to love what you do. - Steve Jobs",
+        "It's never too late to be what you want to be. - George Eliot",
+        "Don’t count the days, make the days count. - Muhammad Ali"
+    )
+}
